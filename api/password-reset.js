6874d2b8d28d346809d64453bb3ee5abd675651d
jiuuuurt5698 <161167137+jiuuuurt5698@users.noreply.github.com>
2025-11-19 13:49:52 +0100

    Update password-reset.js
    Add complete CORS headers to password-reset.js

--- api/password-reset.js
+++ api/password-reset.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
